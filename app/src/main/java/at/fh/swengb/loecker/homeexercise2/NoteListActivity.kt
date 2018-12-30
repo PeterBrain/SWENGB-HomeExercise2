a3a1ab3708478d328377acc02058eb2341c1290b
Peter <peter.loecker@live.at>
2018-12-30 15:20:42 +0100

    shared preferences

---
+++ app/src/main/java/at/fh/swengb/loecker/homeexercise2/NoteListActivity.kt
@@ -0,0 +1,21 @@
+package at.fh.swengb.loecker.homeexercise2
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_note_list)
+
+		val sharedPreferences = getSharedPreferences(packageName, Context.MODE_PRIVATE)
+
+		val user_name = sharedPreferences.getString("UserName", null)
+		val user_age = sharedPreferences.getInt("UserAge", -1)
+
+		user_info.text = "Notes for ${user_name}, ${user_age}"
+	}
+}
